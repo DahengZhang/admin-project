e20ce418ea934b263e72916935f0c47bc9e91d91
DahengZhang <Daheng_Zhang@outlook.com>
2019-12-26 17:30:36 +0800

    first init

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const webpack = require('webpack')
+const { VueLoaderPlugin } = require('vue-loader')
+const HtmlPlugin = require('html-webpack-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+
+module.exports = {
+    devtool: isDev ? 'cheap-source-map' : 'source-maps',
+    entry: './src/index.js',
+    output: {
+        filename: 'app.js',
+        path: path.join(__dirname, 'dist/')
+    },
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            exclude: /node_modules/,
+            loader: 'vue-loader'
+        }, {
+            test: /\.(c|sa|sc)ss$/,
+            exclude: /node_modules/,
+            use: [
+                'vue-style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
+    },
+    devServer: {
+        host: '0.0.0.0',
+        port: 3000,
+        hot: true,
+        overlay: true,
+        historyApiFallback: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new VueLoaderPlugin(),
+        new HtmlPlugin({
+            template: 'template.html',
+            inject: true,
+            minify: {
+                removeComments: false,
+                collapseWhitespace: false
+            }
+        })
+    ]
+}
